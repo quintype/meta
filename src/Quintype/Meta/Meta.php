d18e08f8f4e065047003c4f27bf71706561efe07
shireesha <shireesha@quintype.com>
2016-12-27 11:59:37 +0530

    to add itemprop in og image tag

--- src/Quintype/Meta/Meta.php
+++ src/Quintype/Meta/Meta.php
@@ -134,8 +134,12 @@ class Meta {
      */
     private function metaTag($name, $content)
     {
-        if(substr($name, 0, 3) == 'og:' || substr($name, 0, 3) == 'fb:')
-            return "<meta property=\"$name\" content=\"$content\"/>";
+        if(substr($name, 0, 3) == 'og:' || substr($name, 0, 3) == 'fb:') {
+          if(substr($name, 0, 8) == 'og:image') {
+            return "<meta property=\"$name\" itemprop=\"image\" content=\"$content\"/>";
+          }
+          return "<meta property=\"$name\" content=\"$content\"/>";
+        }
         else if(substr($name, 0, 4) == 'rel:'){
             $name=ltrim($name,"rel:");
             return "<link rel=\"$name\" href=\"$content\"/>";
